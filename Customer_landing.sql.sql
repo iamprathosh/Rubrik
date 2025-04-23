49c916d452760db9286ba6eba89b152f9897ac6f
Guru Prathosh <86939870+iamprathosh@users.noreply.github.com>
2025-04-23 21:14:58 +0530

    firdt

---
+++ Customer_landing.sql.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "mytable" (
+  "CREATE EXTERNAL TABLE `customer_landing`(" text
+);
+
+INSERT INTO "mytable" ("CREATE EXTERNAL TABLE `customer_landing`(")
+VALUES
+('`customername` string COMMENT ''from deserializer'''),
+('`email` string COMMENT ''from deserializer'''),
+('`phone` string COMMENT ''from deserializer'''),
+('`birthday` string COMMENT ''from deserializer'''),
+('`serialnumber` string COMMENT ''from deserializer'''),
+('`registrationdate` bigint COMMENT ''from deserializer'''),
+('`lastupdatedate` bigint COMMENT ''from deserializer'''),
+('`sharewithresearchasofdate` bigint COMMENT ''from deserializer'''),
+('`sharewithpublicasofdate` bigint COMMENT ''from deserializer'''),
+('`sharewithfriendsasofdate` bigint COMMENT ''from deserializer'')'),
+('ROW FORMAT SERDE'),
+('''org.openx.data.jsonserde.JsonSerDe'''),
+('STORED AS INPUTFORMAT'),
+('''org.apache.hadoop.mapred.TextInputFormat'''),
+('OUTPUTFORMAT'),
+('''org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'''),
+('LOCATION'),
+('''s3://stedi-human-sr/customer/landing/'''),
+('TBLPROPERTIES ('),
+('''classification''=''json'')');
---
+++ Step_trainer_landing.sql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "mytable" (
+  "CREATE EXTERNAL TABLE `step_trainer_landing`(" text
+);
+
+INSERT INTO "mytable" ("CREATE EXTERNAL TABLE `step_trainer_landing`(")
+VALUES
+('`sensorreadingtime` bigint COMMENT ''from deserializer'''),
+('`serialnumber` string COMMENT ''from deserializer'''),
+('`distancefromobject` int COMMENT ''from deserializer'')'),
+('ROW FORMAT SERDE'),
+('''org.openx.data.jsonserde.JsonSerDe'''),
+('STORED AS INPUTFORMAT'),
+('''org.apache.hadoop.mapred.TextInputFormat'''),
+('OUTPUTFORMAT'),
+('''org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'''),
+('LOCATION'),
+('''s3://stedi-human-sr/step_trainer/landing/'''),
+('TBLPROPERTIES ('),
+('''classification''=''json'')');
